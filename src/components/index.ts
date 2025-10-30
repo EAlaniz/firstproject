88dbf9f80f0e8c2f6c0ebe17d3c03720097b3c56
Ernie Alaniz <ernieb@MacBookPro.attlocal.net>
2025-10-30 14:36:38 -0700

    new version

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 // Supporting components
 export { default as Modal } from './Modal';
 export { default as StepTracker } from './StepTracker';
-export { EnhancedWalletConnector } from './EnhancedWalletConnector';
 export { ErrorBoundary } from './ErrorBoundary';
  
