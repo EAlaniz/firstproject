f2ce252691b2fa9203d540452107dcfbc8c62aaf
Ernie Alaniz <ernieb@MacBookPro.attlocal.net>
2025-10-31 16:05:59 -0700

    mini app wallet match

--- src/components/OnchainKitWallet.tsx
+++ src/components/OnchainKitWallet.tsx
@@ -3,17 +3,25 @@ import {
   Wallet,
   WalletDropdown,
   WalletDropdownDisconnect,
+  WalletDropdownLink,
 } from '@coinbase/onchainkit/wallet';
 import {
   Address,
   Avatar,
   Name,
   Identity,
+  EthBalance,
 } from '@coinbase/onchainkit/identity';
+import { color } from '@coinbase/onchainkit/theme';
 
 /**
- * OnchainKit Wallet component - matches Base docs pattern exactly
+ * OnchainKit Wallet component - 1:1 match between desktop and mini app
  * https://docs.base.org/onchainkit/wallet/wallet
+ * 
+ * Desktop and mini app now have identical components:
+ * - Identity (Avatar, Name, Address, EthBalance)
+ * - WalletDropdownLink (optional navigation)
+ * - WalletDropdownDisconnect
  */
 export const OnchainKitWallet = () => {
   return (
@@ -23,8 +31,12 @@ export const OnchainKitWallet = () => {
         <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
           <Avatar />
           <Name />
-          <Address className="text-gray-400" />
+          <Address className={color.foregroundMuted} />
+          <EthBalance />
         </Identity>
+        <WalletDropdownLink icon="wallet" href="https://keys.coinbase.com">
+          Wallet
+        </WalletDropdownLink>
         <WalletDropdownDisconnect />
       </WalletDropdown>
     </Wallet>
